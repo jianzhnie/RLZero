512f67c5ff32751e0809ccb88c42486c527839dc
jianzhnie <jianzhnie@126.com>
2023-02-14 15:06:48 +0800

    Update mcts.py

--- examples/mcts_tictactoe/mcts.py
+++ examples/mcts_tictactoe/mcts.py
@@ -1,54 +1,57 @@
-"""
-A minimal implementation of Monte Carlo tree search (MCTS) in Python 3
-Luke Harold Miles, July 2019, Public Domain Dedication
-See also https://en.wikipedia.org/wiki/Monte_Carlo_tree_search
-https://gist.github.com/qpwo/c538c6f73727e254fdc7fab81024f6e1
-"""
+"""A minimal implementation of Monte Carlo tree search (MCTS) in Python 3 Luke
+Harold Miles, July 2019, Public Domain Dedication See also
+https://en.wikipedia.org/wiki/Monte_Carlo_tree_search
+https://gist.github.com/qpwo/c538c6f73727e254fdc7fab81024f6e1."""
+import math
 from abc import ABC, abstractmethod
 from collections import defaultdict
-import math
 
 
 class Node(ABC):
-    """
-    A representation of a single board state.
-    MCTS works by constructing a tree of these Nodes.
-    Could be e.g. a chess or checkers board state.
+    """A representation of a single board state.
+
+    MCTS works by constructing a tree of these Nodes. Could be e.g. a chess or checkers board state.
     """
     @abstractmethod
     def find_children(self):
-        "All possible successors of this board state"
+        """All possible successors of this board state."""
         return set()
 
     @abstractmethod
     def find_random_child(self):
-        "Random successor of this board state (for more efficient simulation)"
+        """Random successor of this board state (for more efficient
+        simulation)"""
         return None
 
     @abstractmethod
     def is_terminal(self):
-        "Returns True if the node has no children"
+        """Returns True if the node has no children."""
         return True
 
     @abstractmethod
     def reward(self):
-        "Assumes `self` is terminal node. 1=win, 0=loss, .5=tie, etc"
+        """Assumes `self` is terminal node.
+
+        1=win, 0=loss, .5=tie, etc
+        """
         return 0
 
     @abstractmethod
     def __hash__(self):
-        "Nodes must be hashable"
+        """Nodes must be hashable."""
         return 123456789
 
     @abstractmethod
     def __eq__(node1, node2):
-        "Nodes must be comparable"
+        """Nodes must be comparable."""
         return True
 
 
 class MCTS:
-    "Monte Carlo tree searcher. First rollout the tree then choose a move."
+    """Monte Carlo tree searcher.
 
+    First rollout the tree then choose a move.
+    """
     def __init__(self, exploration_weight=1):
         self.Q = defaultdict(int)  # total reward of each node
         self.N = defaultdict(int)  # total visit count for each node
@@ -56,22 +59,28 @@ class MCTS:
         self.exploration_weight = exploration_weight
 
     def choose(self, node: Node):
-        "Choose the best successor of node. (Choose a move in the game)"
+        """Choose the best successor of node.
+
+        (Choose a move in the game)
+        """
         if node.is_terminal():
-            raise RuntimeError(f"choose called on terminal node {node}")
+            raise RuntimeError(f'choose called on terminal node {node}')
 
         if node not in self.children:
             return node.find_random_child()
 
         def score(n):
             if self.N[n] == 0:
-                return float("-inf")  # avoid unseen moves
+                return float('-inf')  # avoid unseen moves
             return self.Q[n] / self.N[n]  # average reward
 
         return max(self.children[node], key=score)
 
     def do_rollout(self, node: Node):
-        "Make the tree one layer better. (Train for one iteration.)"
+        """Make the tree one layer better.
+
+        (Train for one iteration.)
+        """
         path = self._select(node)
         leaf = path[-1]
         self._expand(leaf)
@@ -79,7 +88,7 @@ class MCTS:
         self._backpropagate(path, reward)
 
     def _select(self, node: Node):
-        "Find an unexplored descendent of `node`"
+        """Find an unexplored descendent of `node`"""
         path = []
         while True:
             path.append(node)
@@ -94,13 +103,14 @@ class MCTS:
             node = self._uct_select(node)  # descend a layer deeper
 
     def _expand(self, node: Node):
-        "Update the `children` dict with the children of `node`"
+        """Update the `children` dict with the children of `node`"""
         if node in self.children:
             return  # already expanded
         self.children[node] = node.find_children()
 
     def _simulate(self, node: Node):
-        "Returns the reward for a random simulation (to completion) of `node`"
+        """Returns the reward for a random simulation (to completion) of
+        `node`"""
         invert_reward = True
         while True:
             if node.is_terminal():
@@ -110,14 +120,14 @@ class MCTS:
             invert_reward = not invert_reward
 
     def _backpropagate(self, path, reward):
-        "Send the reward back up to the ancestors of the leaf"
+        """Send the reward back up to the ancestors of the leaf."""
         for node in reversed(path):
             self.N[node] += 1
             self.Q[node] += reward
             reward = 1 - reward  # 1 for me is 0 for my enemy, and vice versa
 
     def _uct_select(self, node: Node):
-        "Select a child of node, balancing exploration & exploitation"
+        """Select a child of node, balancing exploration & exploitation."""
 
         # All children of node should already be expanded:
         assert all(n in self.children for n in self.children[node])
@@ -125,7 +135,7 @@ class MCTS:
         log_N_vertex = math.log(self.N[node])
 
         def uct(n):
-            "Upper confidence bound for trees"
+            """Upper confidence bound for trees."""
             return self.Q[n] / self.N[n] + self.exploration_weight * math.sqrt(
                 log_N_vertex / self.N[n])
 
