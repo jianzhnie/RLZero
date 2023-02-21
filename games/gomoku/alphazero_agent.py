aa88e769462e75230976a7b86b231c4513ac869c
jianzhnie <jianzhnie@126.com>
2023-02-21 10:31:57 +0800

    Update alphazero_agent.py

--- games/gomoku/alphazero_agent.py
+++ games/gomoku/alphazero_agent.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 from model import PolicyValueNet
+from torch.autograd import Variable
 
 
 class AlphaZeroAgent(object):
@@ -34,22 +35,34 @@ class AlphaZeroAgent(object):
         current_state = np.ascontiguousarray(board.current_state().reshape(
             -1, 4, self.board_width, self.board_height))
 
-        current_state = torch.from_numpy(current_state).float().to(self.device)
-        log_act_probs, value = self.policy_value_net(current_state)
+        # current_state = torch.from_numpy(current_state).float().to(self.device)
+        # log_act_probs, value = self.policy_value_net(current_state)
+        log_act_probs, value = self.policy_value_net(
+            Variable(torch.from_numpy(current_state).float()))
+
         act_probs = np.exp(log_act_probs.data.numpy().flatten())
         act_probs = zip(legal_positions, act_probs[legal_positions])
-        value = value.items()
+        value = value.data[0][0]
         return act_probs, value
 
     def learn(self, state_batch, mcts_probs, target_vs):
         """perform a training step."""
         # train mode
         self.policy_value_net.train()
+
+        state_batch = np.array(state_batch)
+        mcts_probs = np.array(mcts_probs)
+        target_batch = np.array(target_vs)
+
+        state_batch = Variable(torch.FloatTensor(state_batch))
+        mcts_probs = Variable(torch.FloatTensor(mcts_probs))
+        target_batch = Variable(torch.FloatTensor(target_batch))
+
         log_act_probs, value = self.policy_value_net(state_batch)
 
         # define the loss = (z - v)^2 - pi^T * log(p) + c||theta||^2
         # Note: the L2 penalty is incorporated in optimizer
-        value_loss = F.mse_loss(value.view(-1), target_vs)
+        value_loss = F.mse_loss(value.view(-1), target_batch)
         # policy loss
         policy_loss = -torch.mean(torch.sum(mcts_probs * log_act_probs, 1))
         # total loss
@@ -67,14 +80,16 @@ class AlphaZeroAgent(object):
         return loss.item(), policy_loss.item(), value_loss.item(
         ), entropy.item()
 
-    def predict(self, state):
+    def predict(self, state_batch):
         self.policy_value_net.eval()  # eval mode
+        state_batch = np.array(state_batch)
+        state_batch = Variable(torch.FloatTensor(state_batch))
 
         with torch.no_grad():
-            log_pi, value = self.policy_value_net(state)
+            log_pi, value = self.policy_value_net(state_batch)
 
-        pi = torch.exp(log_pi)
-        return pi, value
+        act_probs = np.exp(log_pi.data.numpy())
+        return act_probs, value.data.numpy()
 
     def save(self,
              save_dir: str,
