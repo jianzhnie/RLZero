54426e91c40fd11da330379dff48f0a984340244
jianzhnie <jianzhnie@126.com>
2023-12-10 21:31:16 +0800

    update rlzero

--- games/gomoku/alphazero_agent.py
+++ games/gomoku/alphazero_agent.py
@@ -4,27 +4,31 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-from model import PolicyValueNet
-from torch.autograd import Variable
+
+from rlzero.model.policy_value_net import PolicyValueNet
 
 
 class AlphaZeroAgent(object):
 
-    def __init__(self,
-                 board_width: int,
-                 board_height: int,
-                 learning_rate: float = 0.001,
-                 weight_decay: float = 1e-4,
-                 device: str = 'cpu') -> None:
+    def __init__(
+        self,
+        board_width: int,
+        board_height: int,
+        learning_rate: float = 0.001,
+        weight_decay: float = 1e-4,
+        device: str = 'cpu',
+    ) -> None:
         self.board_width = board_width
         self.board_height = board_height
 
         self.device = device
         self.policy_value_net = PolicyValueNet(board_width, board_height)
         self.policy_value_net.to(device)
-        self.optimizer = optim.Adam(self.policy_value_net.parameters(),
-                                    lr=learning_rate,
-                                    weight_decay=weight_decay)
+        self.optimizer = optim.Adam(
+            self.policy_value_net.parameters(),
+            lr=learning_rate,
+            weight_decay=weight_decay,
+        )
 
     def policy_value_fn(self, board):
         """
@@ -36,15 +40,22 @@ class AlphaZeroAgent(object):
         current_state = np.ascontiguousarray(board.current_state().reshape(
             -1, 4, self.board_width, self.board_height))
 
-        # current_state = torch.from_numpy(current_state).float().to(self.device)
-        # log_act_probs, value = self.policy_value_net(current_state)
-        log_act_probs, value = self.policy_value_net(
-            Variable(torch.from_numpy(current_state).float()))
-
+        current_state = torch.from_numpy(current_state).float().to(self.device)
+        log_act_probs, value = self.policy_value_net(current_state)
         act_probs = np.exp(log_act_probs.data.numpy().flatten())
         act_probs = zip(legal_positions, act_probs[legal_positions])
         value = value.data[0][0]
         return act_probs, value
+
+    def policy_value(self, state_batch):
+        """
+        input: a batch of states
+        output: a batch of action probabilities and state values
+        """
+        state_batch = torch.FloatTensor(state_batch, device=self.device)
+        log_act_probs, value = self.policy_value_net(state_batch)
+        act_probs = np.exp(log_act_probs.data.cpu().numpy())
+        return act_probs, value.data.cpu().numpy()
 
     def learn(self, state_batch, mcts_probs, target_vs):
         """perform a training step."""
@@ -55,9 +66,10 @@ class AlphaZeroAgent(object):
         mcts_probs = np.array(mcts_probs)
         target_batch = np.array(target_vs)
 
-        state_batch = Variable(torch.FloatTensor(state_batch))
-        mcts_probs = Variable(torch.FloatTensor(mcts_probs))
-        target_batch = Variable(torch.FloatTensor(target_batch))
+        device = self.device
+        state_batch = torch.FloatTensor(state_batch, device)
+        mcts_probs = torch.FloatTensor(mcts_probs, device)
+        target_batch = torch.FloatTensor(target_batch, device)
 
         log_act_probs, value = self.policy_value_net(state_batch)
 
@@ -84,7 +96,7 @@ class AlphaZeroAgent(object):
     def predict(self, state_batch):
         self.policy_value_net.eval()  # eval mode
         state_batch = np.array(state_batch)
-        state_batch = Variable(torch.FloatTensor(state_batch))
+        state_batch = torch.FloatTensor(state_batch, device=self.device)
 
         with torch.no_grad():
             log_pi, value = self.policy_value_net(state_batch)
@@ -92,11 +104,12 @@ class AlphaZeroAgent(object):
         act_probs = np.exp(log_pi.data.numpy())
         return act_probs, value.data.numpy()
 
-    def save(self,
-             save_dir: str,
-             model_name: str = 'model.th',
-             opt_name: str = 'optimizer.th'):
-
+    def save(
+        self,
+        save_dir: str,
+        model_name: str = 'model.th',
+        opt_name: str = 'optimizer.th',
+    ):
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
         model_path = os.path.join(save_dir, model_name)
@@ -105,11 +118,12 @@ class AlphaZeroAgent(object):
         torch.save(self.optimizer.state_dict(), optimizer_path)
         print('save model successfully!')
 
-    def restore(self,
-                save_dir: str,
-                model_name: str = 'model.th',
-                opt_name: str = 'optimizer.th'):
-
+    def restore(
+        self,
+        save_dir: str,
+        model_name: str = 'model.th',
+        opt_name: str = 'optimizer.th',
+    ):
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
         model_path = os.path.join(save_dir, model_name)
