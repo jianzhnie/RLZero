403c1b6da0e0d2f58c63e1e5059c031b58238069
jianzhnie <jianzhnie@126.com>
2023-12-11 17:10:34 +0800

    update rlzero

--- rlzero/mcts/alphazero_player.py
+++ rlzero/mcts/alphazero_player.py
@@ -59,5 +59,5 @@ class AlphaZeroPlayer(Player):
             print('WARNING: the board is full')
 
     def __str__(self):
-        return 'AlphaZeroPlayer {}{}'.format(self.get_player_id()(),
+        return 'AlphaZeroPlayer {}{}'.format(self.get_player_id(),
                                              self.get_player_name())
