8381fc57ff604ce03b30892e126d96591880aec8
jianzhnie <jianzhnie@126.com>
2024-09-24 15:46:45 +0800

    Update rlzero

--- examples/train.py
+++ examples/train.py
@@ -2,11 +2,11 @@ import os
 import sys
 
 sys.path.append(os.getcwd())
-from rlzero.agents.dmc import DistributedDouZero
-from rlzero.agents.rl_args import parse_args
+from rlzero.algorithms.impala.impala_trainer import ImpalaTrainer
+from rlzero.algorithms.rl_args import parse_args
 
 if __name__ == '__main__':
     flags = parse_args()
     print(flags)
-    agent = DistributedDouZero(flags)
+    agent = ImpalaTrainer(flags)
     agent.train()
