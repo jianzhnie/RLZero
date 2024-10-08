8607ea774cdc8beab140129e96790d1f0d691631
jianzhnie <jianzhnie@126.com>
2024-10-08 09:25:00 +0800

    Update rlzero impala

--- examples/test_impala_atari.py
+++ examples/test_impala_atari.py
@@ -2,10 +2,10 @@ import os
 import sys
 
 sys.path.append(os.getcwd())
+from rlzero.algorithms.impala.impala_atari import ImpalaTrainer
 from rlzero.algorithms.rl_args import parse_args
-from RLZero.rlzero.algorithms.impala.impala_atari import ImpalaTrainer
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     flags = parse_args()
     print(flags)
     agent = ImpalaTrainer(flags)
